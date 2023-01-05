ee63b46bc86dced7b20312a1b1cd08e85eb8c7dc
bri <bcamesi@gmail.com>
2023-01-05 16:11:03 -0500

    create components

---
+++ src/components/stocks.js
@@ -0,0 +1,5 @@
+const Stocks = props => {
+    return <h1>Stocks</h1>
+}
+
+export default Stocks;
--- src/router.js
+++ src/router.js
@@ -3,13 +3,29 @@ import {
     createRoutesFromElements,
     Route,
 } from "react-router-dom";
+import App from './App';
+import Main from './components/main'
+import Stocks from './components/stocks';
+import About from './components/about';
 
 const router = createBrowserRouter(
     createRoutesFromElements(
         <Route 
             path="/" 
-            element={<h1>Hello World</h1>}
+            element={<App/>}
         >
+                <Route
+                    path='/'
+                    element={<Main/>}
+                />
+                <Route 
+                    path='/'
+                    element={<Stocks/>}
+                />
+                <Route 
+                    path='/'
+                    element={<About/>}
+                />
         </Route>
     )
 );
